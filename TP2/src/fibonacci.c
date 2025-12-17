7c4bdf5a9c2f9f62dfc7b56f30aa131d6640fbd1
guedji <154362631+guedji@users.noreply.github.com>
2025-12-17 20:00:58 +0100

    Update fibonacci.c

---
+++ TP2/src/fibonacci.c
@@ -0,0 +1,12 @@
+Génération de la Suite de Fibonacci en C
+
+La suite de Fibonacci est une séquence d'entiers définie comme suit :
+
+U0 = 0
+U1 = 1
+Un = Un-1 + Un-2
+L'objectif de l'exercice est d'écrire un programme en C nommé fibonacci.c qui génère et affiche les n termes de la suite de Fibonacci, c'est-à-dire U0, U1, U2, ..., Un. À l'intérieur de ce fichier, écrivez un programme en langage C qui génère les n termes de la suite de Fibonacci. Utilisez une boucle pour calculer les termes de la suite jusqu'à Un en utilisant la formule Un = Un-1 + Un-2. Affichez les termes générés de la suite de Fibonacci à l'écran.
+
+Exemple :
+
+Si l'utilisateur saisit la valeur 7 pour n, le programme devrait afficher la suite de Fibonacci jusqu'à U7 : 0, 1, 1, 2, 3, 5, 8.
