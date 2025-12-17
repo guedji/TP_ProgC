478ad59b35f6f984bda7e70f8f7f7e73d32dd0c2
guedji <154362631+guedji@users.noreply.github.com>
2025-12-17 20:16:55 +0100

    Update ptrvariables.c

--- TP2/src/ptrvariables.c
+++ TP2/src/ptrvariables.c
@@ -1,27 +1,31 @@
-Manipulation des Variables de Types de Base avec des Pointeurs
+#include <stdio.h>
 
-L'objectif de cet exercice est de réutiliser le programme variables.c en créant un nouveau programme en C nommé ptrvariables.c. Ce programme doit affecter et afficher les valeurs des variables de différents types de base (char, short, int, long int, long long int, float, double, long double) en utilisant leurs adresses avec des pointeurs. En outre, le programme doit afficher à la fois l'adresse et la valeur de ces variables avant et après la manipulation par leurs adresses en utilisant les pointeurs.
+int main() {
 
-Instructions :
+    /* Déclaration des variables */
+    char c = 0x12;
+    short s = 0x1234;
+    int i = 0xA47865FF;
+    long int li = 0x12345678;
+    long long int lli = 0x1122334455667788;
+    float f = 2.0f;
+    double d = 4.0;
+    long double ld = 8.0L;
 
-Réutilisez les variables de différents types de base (char, short, int, long int, long long int, float, double, long double) à partir du programme variables.c.
-Utilisez des pointeurs pour stocker les adresses de ces variables.
-Avant la manipulation des variables par leurs adresses, affichez à l'écran l'adresse et la valeur de chaque variable en hexadécimal.
-Effectuez la manipulation des variables en utilisant les pointeurs (par exemple, modifiez la valeur d'une variable via son pointeur).
-Après la manipulation, réaffichez à l'écran l'adresse et la valeur actualisée de chaque variable en hexadécimal.
-Assurez-vous que les valeurs sont affichées en hexadécimal, comme indiqué dans l'exemple.
-Exemple :
+    /* Déclaration des pointeurs */
+    char *pc = &c;
+    short *ps = &s;
+    int *pi = &i;
+    long int *pli = &li;
+    long long int *plli = &lli;
+    float *pf = &f;
+    double *pd = &d;
+    long double *pld = &ld;
 
-Un exemple d'utilisation de ce programme pourrait être :
+    printf("=== Avant la manipulation ===\n");
 
-Le programme affiche les adresses et les valeurs des variables avant et après la manipulation par les pointeurs, par exemple :
+    printf("Adresse c  : %p | Valeur : %02x\n", (void*)pc, (unsigned char)c);
+    printf("Adresse s  : %p | Valeur : %04x\n", (void
 
-Avant la manipulation :
-Adresse de i : 0x7ffd779efc50, Valeur de i : a47865ff
-Adresse de f : 0x7ffd779efc4c, Valeur de f : 40000000
 
-Après la manipulation :
-Adresse de i : 0x7ffd779efc50, Valeur de i : a47865fe
-Adresse de f : 0x7ffd779efc4c, Valeur de f : 3f800000
-Assurez-vous de respecter les consignes en utilisant des pointeurs pour manipuler les variables et en affichant les adresses et les valeurs en hexadécimal.
 
