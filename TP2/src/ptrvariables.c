f38c16c62c7901f7b83ad86b6d6c771f80ce4397
guedji <154362631+guedji@users.noreply.github.com>
2025-12-17 20:18:19 +0100

    Update ptrvariables.c

--- TP2/src/ptrvariables.c
+++ TP2/src/ptrvariables.c
@@ -25,7 +25,38 @@ int main() {
     printf("=== Avant la manipulation ===\n");
 
     printf("Adresse c  : %p | Valeur : %02x\n", (void*)pc, (unsigned char)c);
-    printf("Adresse s  : %p | Valeur : %04x\n", (void
+    printf("Adresse s  : %p | Valeur : %04x\n", (void*)ps, (unsigned short)s);
+    printf("Adresse i  : %p | Valeur : %08x\n", (void*)pi, i);
+    printf("Adresse li : %p | Valeur : %lx\n", (void*)pli, li);
+    printf("Adresse lli: %p | Valeur : %llx\n", (void*)plli, lli);
+    printf("Adresse f  : %p | Valeur : %08x\n", (void*)pf, *(unsigned int*)pf);
+    printf("Adresse d  : %p | Valeur : %016llx\n", (void*)pd, *(unsigned long long*)pd);
+    printf("Adresse ld : %p | Valeur : %016llx\n", (void*)pld, *(unsigned long long*)pld);
+
+    /* Manipulation via pointeurs */
+    *pc += 1;
+    *ps += 1;
+    *pi -= 1;
+    *pli += 1;
+    *plli -= 1;
+    *pf = 1.0f;
+    *pd = 1.0;
+    *pld = 1.0L;
+
+    printf("\n=== Apres la manipulation ===\n");
+
+    printf("Adresse c  : %p | Valeur : %02x\n", (void*)pc, (unsigned char)c);
+    printf("Adresse s  : %p | Valeur : %04x\n", (void*)ps, (unsigned short)s);
+    printf("Adresse i  : %p | Valeur : %08x\n", (void*)pi, i);
+    printf("Adresse li : %p | Valeur : %lx\n", (void*)pli, li);
+    printf("Adresse lli: %p | Valeur : %llx\n", (void*)plli, lli);
+    printf("Adresse f  : %p | Valeur : %08x\n", (void*)pf, *(unsigned int*)pf);
+    printf("Adresse d  : %p | Valeur : %016llx\n", (void*)pd, *(unsigned long long*)pd);
+    printf("Adresse ld : %p | Valeur : %016llx\n", (void*)pld, *(unsigned long long*)pld);
+
+    return 0;
+}
+
 
 
 
