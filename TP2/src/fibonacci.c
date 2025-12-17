e1f77ba2c1a50d82c03b4391f9c1f08f100782f3
guedji <154362631+guedji@users.noreply.github.com>
2025-12-17 20:03:02 +0100

    Update fibonacci.c

--- TP2/src/fibonacci.c
+++ TP2/src/fibonacci.c
@@ -1,12 +1,23 @@
-Génération de la Suite de Fibonacci en C
+#include <stdio.h>
 
-La suite de Fibonacci est une séquence d'entiers définie comme suit :
+int main() {
+    int d = 0x10001000;   // valeur de test
+    int bit4, bit20;
 
-U0 = 0
-U1 = 1
-Un = Un-1 + Un-2
-L'objectif de l'exercice est d'écrire un programme en C nommé fibonacci.c qui génère et affiche les n termes de la suite de Fibonacci, c'est-à-dire U0, U1, U2, ..., Un. À l'intérieur de ce fichier, écrivez un programme en langage C qui génère les n termes de la suite de Fibonacci. Utilisez une boucle pour calculer les termes de la suite jusqu'à Un en utilisant la formule Un = Un-1 + Un-2. Affichez les termes générés de la suite de Fibonacci à l'écran.
+    // Extraction du 4e bit de gauche (bit 28)
+    bit4 = (d >> 28) & 1;
 
-Exemple :
+    // Extraction du 20e bit de gauche (bit 12)
+    bit20 = (d >> 12) & 1;
 
-Si l'utilisateur saisit la valeur 7 pour n, le programme devrait afficher la suite de Fibonacci jusqu'à U7 : 0, 1, 1, 2, 3, 5, 8.
+    // Vérification des deux bits
+    if (bit4 == 1 && bit20 == 1) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
+    return 0;
+}
+
+
