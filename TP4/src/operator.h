306e7cc13d3c709d7b07efca2dfc6595fc8283c2
guedji <154362631+guedji@users.noreply.github.com>
2025-12-17 20:50:23 +0100

    Update operator.h

---
+++ TP4/src/operator.h
@@ -0,0 +1,14 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+/* Déclarations des fonctions */
+int somme(int a, int b);
+int difference(int a, int b);
+int produit(int a, int b);
+int quotient(int a, int b);
+int modulo(int a, int b);
+int et_bit(int a, int b);
+int ou_bit(int a, int b);
+int negation(int a);
+
+#endif
